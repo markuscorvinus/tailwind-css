d59cee02b24af71e123be3c8e55019534250e7a5
Markus Corvinus <markus.corvinus0827@gmail.com>
2023-07-30 02:58:36 +0800

    spacing and sizes done

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='whitespace-nowrap'>
-      <h1 className='text-xl itali'>Title 1</h1>
-      <h2 className='text-xl'>Title 2</h2>
-      <h3 className='text-xl'>Title 3</h3>
-      <p className='text-base leading-loose underline decoration-red-600 decoration-wavy decoration-1 underline-offset-4'>A regular paragraph</p>
-      <p className='text-sm'>A description paragraph</p>
-      <p className='note text-xs'>A little note</p>
-      <p className=''>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <div className='flex min-h-screen space-x-3'>
+      {/* <div className='bg-red-500 text-white font-bold p-4'>Padding</div>
+      <div className='bg-blue-500 text-white font-bold my-4'>Margin</div> */}
+      {/* <h1 className='text-2xl font-bold mb-2'>This is a Title</h1>
+      <p>This is a paragraph</p> */}
+      {/* <div class="w-1/5  bg-red-400 ">Sidebar</div>
+      <div class="w-4/5 bg-blue-400">Main</div> */}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    spacing: {
-      '1': '8px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px',
-    },
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
+    // spacing: {
+    //   '1': '8px',
+    //   '2': '12px',
+    //   '3': '16px',
+    //   '4': '24px',
+    //   '5': '32px',
+    //   '6': '48px',
+    // },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
