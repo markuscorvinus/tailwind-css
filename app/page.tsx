de62c8e119228c5de72e28b85b0778fb3df63777
Light-DevPC <markus.corvinus0827@gmail.com>
2023-07-30 00:15:35 +0800

    WIP-typography

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-green-500 text-white border-8">
-        Hello Test
-      </h1>
-      <h1 className="text-yellow-500">
-        Hello Yellow
-      </h1>
+      <h1>Title 1</h1>
+      <h2>Title 2</h2>
+      <h3>Title 3</h3>
+      <p className='text-base'>A regular paragraph</p>
+      <p className='text-sm'>A description paragraph</p>
+      <p className='note text-xs'>A little note</p>
     </div>
   )
 }
