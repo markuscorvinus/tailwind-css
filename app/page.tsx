b332f3f77b2ddd892b511a7d32db2f4929bf11f6
Markus Corvinus <markus.corvinus0827@gmail.com>
2023-07-30 15:33:34 +0800

    flex box done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen space-x-3'>
-      {/* <div className='bg-red-500 text-white font-bold p-4'>Padding</div>
-      <div className='bg-blue-500 text-white font-bold my-4'>Margin</div> */}
-      {/* <h1 className='text-2xl font-bold mb-2'>This is a Title</h1>
-      <p>This is a paragraph</p> */}
-      {/* <div class="w-1/5  bg-red-400 ">Sidebar</div>
-      <div class="w-4/5 bg-blue-400">Main</div> */}
+    <div className="Parent">
+      <div>Header</div>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-32 basis-1/4 bg-sky-500 ">Sidebar</div>
+        <div className="w-full basis-3/4 bg-red-600">MainContent</div>
+      </div>
     </div>
-  )
+  );
 }
