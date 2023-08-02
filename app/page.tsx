99a5e588966ad77266b911e54dea4394f6513fd8
Markus Corvinus <markus.corvinus0827@gmail.com>
2023-08-02 15:21:12 +0800

    filters done

--- app/page.tsx
+++ app/page.tsx
@@ -2,63 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="Parent h-screen bg-white p-4 text-white">
-      <div className="block rounded-lg border bg-white p-4 text-black shadow-md">
-        <h1 className="text-2xl">Block</h1>
+    <div className="Parent h-screen bg-white p-4 ">
+      <div className="inline-block rounded-lg border bg-white p-4 text-black shadow-md">
+        <div className="relative">
+          <div className="absolute inset-6 h-20 w-20 bg-white/30 backdrop-blur-sm" />
+          <img
+            src="https://source.unsplash.com/K_TbABnVzHo"
+            alt=""
+            className="mb-4 w-64 backdrop-blur-sm"
+          />
+        </div>
+
+        <h1 className="text-2xl">Hello</h1>
         <p className="mb-4">This is my Div</p>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-      </div>
-      <div className="mx-4 inline-block rounded-lg border bg-white p-4 text-black shadow-lg">
-        <h1 className="text-2xl">Inline-Block</h1>
-        <p className="mb-4">This is my Div</p>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-      </div>
-      <div className="inline-flex rounded-lg border bg-white p-4 text-black shadow-md">
-        <h1 className="text-2xl">Inline Flex</h1>
-        <p className="mb-4">This is my Div</p>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-      </div>
-      <div className="grid rounded-lg border bg-white p-4 text-black shadow-md">
-        <h1 className="text-2xl">Grid</h1>
-        <p className="mb-4">This is my Div</p>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-      </div>
-      <div className="inline-grid rounded-lg border bg-white p-4 text-black shadow-md">
-        <h1 className="text-2xl">Inline-Grid</h1>
-        <p className="mb-4">This is my Div</p>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
-      </div>
-      <div className="contents rounded-lg border bg-white p-4 text-black shadow-md">
-        <h1 className="text-2xl">Contents</h1>
-        <p className="mb-4">This is my Div</p>
-        <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
-          Say Hello
-        </button>
         <button className="cursor-pointer rounded-lg bg-cyan-500 px-3 py-2">
           Say Hello
         </button>
